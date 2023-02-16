d98f7ccca2a381f85fc30c77b25d99a99135a442
punjabinuclei <guppi0204@gmail.com>
2023-02-16 12:11:26 +0530

    push1

---
+++ index.js
@@ -0,0 +1,96 @@
+
+
+
+
+
+
+$('.owl-1').owlCarousel({
+    loop: true,
+    margin: 10,
+    // autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
+
+
+$('.owl-2').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 1,
+            nav: false
+        },
+        1000: {
+            items: 1,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
+
+$('.owl-3').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
+
+
+
+
+ScrollReveal().reveal('#stats', { delay: 1000 });
+ScrollReveal().reveal('#sucessStory', { delay: 1000 });
+ScrollReveal().reveal('#best1', { delay: 1000 });
+ScrollReveal().reveal('#best2', { delay: 1000 });
+ScrollReveal().reveal('#best3', { delay: 1000 });
+ScrollReveal().reveal('#founder1', { delay: 1000 });
+ScrollReveal().reveal('#founder2', { delay: 1000 });
+ScrollReveal().reveal('#vision', { delay: 1000 });
+ScrollReveal().reveal('#faculty1', { delay: 1000 });
+ScrollReveal().reveal('#faculty2', { delay: 1000 });
+ScrollReveal().reveal('#achieve1', { delay: 1000 });
+ScrollReveal().reveal('#achieve2', { delay: 1000 });
+ScrollReveal().reveal('#testimonials1', { delay: 1000 });
+ScrollReveal().reveal('#testimonials2', { delay: 1000 });
+ScrollReveal().reveal('#gallery1', { delay: 1000 });
+ScrollReveal().reveal('#gallery2', { delay: 1000 });
+ScrollReveal().reveal('#footer', { delay: 1000 });
